05264aace96d0ea928ac73965e4bbb01a67d9368
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-10-20 16:44:58 +0200

    All the bugs fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import MainPage from './Pages/MainPage'
 import './App.css'
 
 function App() {
-
   return (
       <>
       <Paper bg="blue.8" w="100%" p={50} ta="center" mb={40}><Title>Welcome to CTF</Title></Paper>
       <Box w="75%" m="auto">
-        <BrowserRouter>
+        <BrowserRouter basename='/CTF'>
           <Routes>
             <Route path='/' element={<MainPage />} />
           </Routes>

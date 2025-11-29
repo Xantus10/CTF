c72829535097851d45180e5a08a9d5839551e64e
xantus10 <jaroslav.zaba@post.cz>
2025-11-29 11:41:05 +0100

    Added Flag regex + notifications

--- src/Pages/Components/Challenge.tsx
+++ src/Pages/Components/Challenge.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Stack, Group, TextInput, Button, Title, Text, Popover, Anchor } from "@mantine/core";
 import { SHA256 } from "crypto-js";
+import { notifications } from "@mantine/notifications";
+import { FaCheckCircle, FaExclamationTriangle } from "react-icons/fa";
 
 /**
  * Props for the Challange component
@@ -20,6 +22,25 @@ export interface ChallangeProps {
    * Hash of the correct solution
    */
   solutionHash: string;
+
+  /**
+   * Flag format options
+   */
+  flagFormat: {
+    /**
+     * RegEx to be matched
+     */
+    regex: RegExp;
+
+    /**
+     * Optional custom flag type message (should end with a space)
+     * 
+     * @description Regex will be appended right after the message
+     * 
+     * @default 'Flag is in format: '
+     */
+    message?: string;
+  }
 
   /**
    * Optional challange files
@@ -42,25 +63,32 @@ export interface ChallangeProps {
   hints?: string[];
 };
 
-function Challenge({title, content, solutionHash, files, hints}: ChallangeProps) {
-  const [inp, setInp] = useState("")
-  const [bgCol, setBgCol] = useState("var(--mantine-color-dark-4)")
+function Challenge({title, content, solutionHash, flagFormat, files, hints}: ChallangeProps) {
+  const [inp, setInp] = useState("");
+  const [bgCol, setBgCol] = useState("var(--mantine-color-dark-4)");
 
   if (files === undefined) files = [];
   if (hints === undefined) hints = [];
 
+  const LOCAL_STORAGE_NAME = SHA256(title+solutionHash).toString();
+
   function checkSolution() {
-    console.log(inp);
+    if (!inp.match(flagFormat.regex)) {
+      notifications.show({message: 'Flag does not match its RegEx!', color: "red", icon: <FaExclamationTriangle />});
+      return;
+    }
     if (solutionHash === SHA256(inp).toString()) {
-      setBgCol("var(--mantine-color-green-9)")
-      localStorage.setItem(solutionHash, inp);
+      setBgCol("var(--mantine-color-green-9)");
+      localStorage.setItem(LOCAL_STORAGE_NAME, inp);
+      notifications.show({message: `Congratulations, you've solved challenge ${title}`, color: "green", icon: <FaCheckCircle />});
     } else {
-      setBgCol("var(--mantine-color-red-9)")
+      setBgCol("var(--mantine-color-red-9)");
+      notifications.show({message: 'Incorrect flag!', color: "red", icon: <FaExclamationTriangle />});
     }
   }
 
   useEffect(() => {
-    let loc = localStorage.getItem(solutionHash);
+    let loc = localStorage.getItem(LOCAL_STORAGE_NAME);
     if (loc) {
       setInp(loc);
       setBgCol("var(--mantine-color-green-9)");
@@ -73,6 +101,7 @@ function Challenge({title, content, solutionHash, files, hints}: ChallangeProps)
     <Stack justify="center" gap="md" p={10} bg="dark.5">
       <Title>{title}</Title>
       {content}
+      <Text>{(flagFormat.message) ? flagFormat.message : "Flag is in format: "}{flagFormat.regex.source}</Text>
       {(files.length !== 0) ? <Text>Files to download</Text> : <></>}
       <Group gap={10}>
         {files.map((val) => {return <Anchor key={val.name} href={val.path} download={val.name}>{val.name}</Anchor>})}
@@ -82,7 +111,7 @@ function Challenge({title, content, solutionHash, files, hints}: ChallangeProps)
         {hints.map((val, ix) => {return <Popover key={ix} position="bottom"><Popover.Target><Button>{`Hint ${ix+1}`}</Button></Popover.Target><Popover.Dropdown styles={{dropdown: {backgroundColor: 'var(--mantine-color-dark-4)'}}}><Text>{val}</Text></Popover.Dropdown></Popover>})}
       </Group>
       <Group>
-        <TextInput flex={5} styles={{input: {backgroundColor: bgCol, color: "whitesmoke"}}} label="Flag" placeholder="flag" value={inp} onChange={(e) => setInp(e.currentTarget.value)} />
+        <TextInput flex={5} styles={{input: {backgroundColor: bgCol, color: "whitesmoke"}}} label="Flag" placeholder={flagFormat.regex.source} value={inp} onChange={(e) => setInp(e.currentTarget.value)} />
         <Button flex={1} onClick={checkSolution}>Submit</Button>
       </Group>
     </Stack>
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
 import App from './App.tsx'
 
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 
 import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider forceColorScheme='dark'>
+      <Notifications position='bottom-right' autoClose={5000} />
       <App />
     </MantineProvider>
   </StrictMode>,

